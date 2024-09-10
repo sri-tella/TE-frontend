8daf5b4bf70df30469ee871e720f557289a4c27c
tsmanjusha5 <tellasrimanjusha@gmail.com>
2024-09-10 10:11:57 -0500

    update

--- src/register.js
+++ src/register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './auth.css';
+import './pages/Login/auth.css';
 
 const Signup = () => {
   const [firstname, setFirstName] = useState('');
