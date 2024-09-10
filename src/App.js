673b72e29056c6b03b45985f6da06278b20feb00
tsmanjusha5 <tellasrimanjusha@gmail.com>
2024-09-10 10:33:16 -0500

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import SelectedRecommendations from "./pages/Evaluation/recommendations";
 import ReviewRecommendations from "./pages/Evaluation/reviewrecommendations";
 import EditReport from "./pages/Evaluation/editreport";
 import Login from "./pages/Login/login";
-import Signup from "./register"
+import Signup from "./pages/Login/register"
 
 function App() {
 
