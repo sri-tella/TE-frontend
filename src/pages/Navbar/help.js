0e08ff542c586be30bdafe115a9cb32684341979
tsmanjusha5 <tellasrimanjusha@gmail.com>
2024-10-01 12:52:12 -0500

    Fixed verbiage

--- src/pages/Navbar/help.js
+++ src/pages/Navbar/help.js
@@ -10,17 +10,17 @@ const Help = () => {
         <div className="help-container">
           <h1 className="home-heading">Help & Instructions</h1>
           <div className="instructions">
-            <h2>How to Start an Evaluation</h2>
+            <h2>How to Start an Observation</h2>
             <ol>
               <li>Log in to the application using your credentials.</li>
-              <li>Navigate to the <strong>Start Evaluation</strong> page from the main menu.</li>
+              <li>Navigate to the <strong>Start Observation</strong> page from the main menu.</li>
               <li>Select the course and fill out the required fields in the form.</li>
-              <li>Click on <strong>Save</strong> to submit your evaluation.</li>
+              <li>Click on <strong>Save</strong> to submit your Observation.</li>
             </ol>
 
-            <h2>Steps to Complete an Evaluation</h2>
+            <h2>Steps to Complete an Observation</h2>
             <ol>
-              <li>After starting an evaluation, you'll be directed to the evaluation form.</li>
+              <li>After starting an Observation, you'll be directed to the Observation form.</li>
               <li>Complete each section of the form by selecting the relevant options and providing feedback.</li>
               <li>Review your selections and feedback before finalizing.</li>
               <li>Click <strong>Save and Continue</strong> to save your progress.</li>
