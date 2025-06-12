5040aa7891a5781aa1d54b855419b7bfe10d1fd1
sri-tella <tellasrimanjusha@gmail.com>
2025-06-12 07:31:11 -0500

    Update intro.js

--- src/pages/Evaluation/intro.js
+++ src/pages/Evaluation/intro.js
@@ -55,7 +55,7 @@ const CombinedForm = () => {
 
     try {
           console.log(formData);
-          await axios.post('http://localhost:8080/api/form/instructor', formData);
+          await axios.post('https://te-backend-production.up.railway.app/api/form/instructor', formData);
 //          alert("Instructor information saved successfully!");
           localStorage.setItem('instructorFormSubmitted', 'true');
           navigate('/inshome');
