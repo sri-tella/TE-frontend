9a104ca506d39657a47976a020b7b8ef120ba7ec
Kandabarov <Vitalii_Kandabarov1@baylor.edu>
2026-02-11 14:14:05 -0600

    fix ai and constant

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8080";
-// export const API_BASE_URL = "https://te-backend-production.up.railway.app";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://te-backend-production.up.railway.app";
